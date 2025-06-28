ae840923ec59d73adc7b56fc5ef475fd683c9b7a
Sparshak Nag <riley.onduty@gmail.com>
2025-06-28 20:47:44 +0530

    monorepo format

--- packages/collector/src/sdk.ts
+++ packages/collector/src/sdk.ts
@@ -3,7 +3,7 @@ import { generateHashedFingerprint } from "./utils/hash.util.js";
 import { collectNavigatorData } from "./utils/navigator.util.js";
 import { collectScreenData } from "./utils/screen.util.js";
 
-export const collect = async() => {
+export const collect = async(): Promise<string> => {
   const canvas = collectCanvasFingerpint();
   const navigatorData = collectNavigatorData();
   const screenData = collectScreenData();
@@ -18,6 +18,5 @@ export const collect = async() => {
   });
 
   //make API call to backend
-  console.log(hash)
   return hash
 }
